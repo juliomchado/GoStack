9c5f8a013ff0f526830db8ec07137c5f4aa953f7
juliomchado <juliocarlos00@hotmail.com>
2020-06-16 00:09:50 +0100

    Simulation http methods

--- GoStack/GoStack-Node/backend/src/index.js
+++ GoStack/GoStack-Node/backend/src/index.js
@@ -2,11 +2,33 @@ const express = require('express')
 
 const app = express()
 
+
+
 app.get('/projects', (req, res) => {
-    return res.json({messege: 'Hello World'})
+    return res.json([
+        'Project 1',
+        'Project 2'
+    ])
+})
+
+app.post('/projects', (req, res) => {
+    return res.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ])
+})
+
+
+app.put('/projects/:id', (req, res) => {
+    return res.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ])
 })
 
 app.listen(3333, () => {
-    console.log('Servidor rodando na porta 3333 🤖')
+    console.log('Servidor rodando na porta 3333 ✨')
 })
 
