9abdce5085b7a31b31aca4ad6bd7d816404782b3
juliomchado <juliocarlos00@hotmail.com>
2020-06-16 16:04:04 +0100

    Middlewares and examples done

--- GoStack/GoStack-Node/backend/src/index.js
+++ GoStack/GoStack-Node/backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express()
 
@@ -7,7 +7,38 @@ app.use(express.json())
 
 const projects = [];
 
+
+function logRequests(req, res, next){
+    const { method, url} = req;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log('Passo 1')
+    console.time(logLabel)
+    
+    next() //Prox middleware
+
+    console.log('Passo 2')
+    console.timeEnd(logLabel)
+} 
+
+function validadeProjectId(req, res, next){
+    const { id} = req.params
+
+    if(!isUuid(id)){
+        return res.status(400).json({error: 'Invalid project ID.'})
+    }
+
+    return next()
+}
+
+app.use(logRequests)
+
+app.use('/project/:id', validadeProjectId)
+
 app.get('/projects', (req, res) => {
+
+    console.log('Passo 3')
 
     const { title} = req.query
 
