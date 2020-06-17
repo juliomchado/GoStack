ea44db4c94e2a10a1e4b49b1a9eeda4b97b4de58
juliomchado <juliocarlos00@hotmail.com>
2020-06-17 23:45:00 +0100

    List api project

--- GoStack/GoStack-React/frontend/src/App.js
+++ GoStack/GoStack-React/frontend/src/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import './App.css';
-import backgroundImage from './assets/backgroud.jpg'
+// import backgroundImage from './assets/backgroud.jpg'
 
 import Header from './components/Header'
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end Web'])
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+
+        api.get('projects').then(res => {
+            setProjects(res.data)
+
+        })
+
+    }, [])
 
     function handleAddProject() {
         setProjects([...projects, `Novo projeto ${Date.now()}`])
 
         console.log(projects)
-    } 
+    }
+
 
     return (
         <>
             <Header title="Projects" />
 
-            <img width={300}  src={backgroundImage} alt=""/>
+            {/* <img width={300}  src={backgroundImage} alt=""/> */}
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
