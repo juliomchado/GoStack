0c7381a5c91526544748b3eb70a1e5d092317726
juliomchado <juliocarlos00@hotmail.com>
2020-06-22 15:38:16 +0100

    Structure configured

---
+++ GoStack/GoStack-FirstProjectNode/Project/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+    return response.json({ messege: 'Hello World' })
+})
+
+app.listen(3333, () => {
+    console.log(`Server started on port 3333🎉`)
+})
