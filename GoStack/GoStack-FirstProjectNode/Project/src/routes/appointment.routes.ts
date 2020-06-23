3129b3404e16c0c3201f6589583bb81a8630bb71
juliomchado <juliocarlos00@hotmail.com>
2020-06-23 23:59:48 +0100

    DTO concepts and typescript Omit

--- GoStack/GoStack-FirstProjectNode/Project/src/routes/appointment.routes.ts
+++ GoStack/GoStack-FirstProjectNode/Project/src/routes/appointment.routes.ts
@@ -24,7 +24,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ messege: 'This appointment is already booked' })
   };
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({ provider, date: parsedDate });
 
   return response.json(appointment);
 });
